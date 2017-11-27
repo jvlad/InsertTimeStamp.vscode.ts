1782c163b615d6d6ab04353b22999ca2ada697a0
teriyakigod2 <shulhindvst@gmail.com>
2017-11-27 09:05:16 +0300

    selecting locales

--- src/extension.ts
+++ src/extension.ts
@@ -1,40 +1,40 @@
 'use strict';
 import * as vscode from 'vscode';
-
-function composeTimeStamp(date: Date): string {
-    let prefix: string = "######"
-    let weekDayName: string = getTodayWeekDayName(date);
-    let monthName: string = geTodayMonthName(date);
-    let monthDay: string = date.getDate().toString();
-    let time: string = date.toLocaleTimeString('en-US', { hour12: false });
-    let timeZoneAbbr: string = getTimeZoneAbbreviation();
-    let year: string = date.getFullYear().toString();
+const timezone = require('moment-timezone');
+const ISOCountries = Object.keys({"AF":"Afghanistan","AX":"Aland Islands","AL":"Albania","DZ":"Algeria","AS":"American Samoa","AD":"Andorra","AO":"Angola","AI":"Anguilla","AQ":"Antarctica","AG":"Antigua And Barbuda","AR":"Argentina","AM":"Armenia","AW":"Aruba","AU":"Australia","AT":"Austria","AZ":"Azerbaijan","BS":"Bahamas","BH":"Bahrain","BD":"Bangladesh","BB":"Barbados","BY":"Belarus","BE":"Belgium","BZ":"Belize","BJ":"Benin","BM":"Bermuda","BT":"Bhutan","BO":"Bolivia","BA":"Bosnia And Herzegovina","BW":"Botswana","BV":"Bouvet Island","BR":"Brazil","IO":"British Indian Ocean Territory","BN":"Brunei Darussalam","BG":"Bulgaria","BF":"Burkina Faso","BI":"Burundi","KH":"Cambodia","CM":"Cameroon","CA":"Canada","CV":"Cape Verde","KY":"Cayman Islands","CF":"Central African Republic","TD":"Chad","CL":"Chile","CN":"China","CX":"Christmas Island","CC":"Cocos (Keeling) Islands","CO":"Colombia","KM":"Comoros","CG":"Congo","CD":"Congo, Democratic Republic","CK":"Cook Islands","CR":"Costa Rica","CI":"Cote D'Ivoire","HR":"Croatia","CU":"Cuba","CY":"Cyprus","CZ":"Czech Republic","DK":"Denmark","DJ":"Djibouti","DM":"Dominica","DO":"Dominican Republic","EC":"Ecuador","EG":"Egypt","SV":"El Salvador","GQ":"Equatorial Guinea","ER":"Eritrea","EE":"Estonia","ET":"Ethiopia","FK":"Falkland Islands (Malvinas)","FO":"Faroe Islands","FJ":"Fiji","FI":"Finland","FR":"France","GF":"French Guiana","PF":"French Polynesia","TF":"French Southern Territories","GA":"Gabon","GM":"Gambia","GE":"Georgia","DE":"Germany","GH":"Ghana","GI":"Gibraltar","GR":"Greece","GL":"Greenland","GD":"Grenada","GP":"Guadeloupe","GU":"Guam","GT":"Guatemala","GG":"Guernsey","GN":"Guinea","GW":"Guinea-Bissau","GY":"Guyana","HT":"Haiti","HM":"Heard Island & Mcdonald Islands","VA":"Holy See (Vatican City State)","HN":"Honduras","HK":"Hong Kong","HU":"Hungary","IS":"Iceland","IN":"India","ID":"Indonesia","IR":"Iran, Islamic Republic Of","IQ":"Iraq","IE":"Ireland","IM":"Isle Of Man","IL":"Israel","IT":"Italy","JM":"Jamaica","JP":"Japan","JE":"Jersey","JO":"Jordan","KZ":"Kazakhstan","KE":"Kenya","KI":"Kiribati","KR":"Korea","KW":"Kuwait","KG":"Kyrgyzstan","LA":"Lao People's Democratic Republic","LV":"Latvia","LB":"Lebanon","LS":"Lesotho","LR":"Liberia","LY":"Libyan Arab Jamahiriya","LI":"Liechtenstein","LT":"Lithuania","LU":"Luxembourg","MO":"Macao","MK":"Macedonia","MG":"Madagascar","MW":"Malawi","MY":"Malaysia","MV":"Maldives","ML":"Mali","MT":"Malta","MH":"Marshall Islands","MQ":"Martinique","MR":"Mauritania","MU":"Mauritius","YT":"Mayotte","MX":"Mexico","FM":"Micronesia, Federated States Of","MD":"Moldova","MC":"Monaco","MN":"Mongolia","ME":"Montenegro","MS":"Montserrat","MA":"Morocco","MZ":"Mozambique","MM":"Myanmar","NA":"Namibia","NR":"Nauru","NP":"Nepal","NL":"Netherlands","AN":"Netherlands Antilles","NC":"New Caledonia","NZ":"New Zealand","NI":"Nicaragua","NE":"Niger","NG":"Nigeria","NU":"Niue","NF":"Norfolk Island","MP":"Northern Mariana Islands","NO":"Norway","OM":"Oman","PK":"Pakistan","PW":"Palau","PS":"Palestinian Territory, Occupied","PA":"Panama","PG":"Papua New Guinea","PY":"Paraguay","PE":"Peru","PH":"Philippines","PN":"Pitcairn","PL":"Poland","PT":"Portugal","PR":"Puerto Rico","QA":"Qatar","RE":"Reunion","RO":"Romania","RU":"Russian Federation","RW":"Rwanda","BL":"Saint Barthelemy","SH":"Saint Helena","KN":"Saint Kitts And Nevis","LC":"Saint Lucia","MF":"Saint Martin","PM":"Saint Pierre And Miquelon","VC":"Saint Vincent And Grenadines","WS":"Samoa","SM":"San Marino","ST":"Sao Tome And Principe","SA":"Saudi Arabia","SN":"Senegal","RS":"Serbia","SC":"Seychelles","SL":"Sierra Leone","SG":"Singapore","SK":"Slovakia","SI":"Slovenia","SB":"Solomon Islands","SO":"Somalia","ZA":"South Africa","GS":"South Georgia And Sandwich Isl.","ES":"Spain","LK":"Sri Lanka","SD":"Sudan","SR":"Suriname","SJ":"Svalbard And Jan Mayen","SZ":"Swaziland","SE":"Sweden","CH":"Switzerland","SY":"Syrian Arab Republic","TW":"Taiwan","TJ":"Tajikistan","TZ":"Tanzania","TH":"Thailand","TL":"Timor-Leste","TG":"Togo","TK":"Tokelau","TO":"Tonga","TT":"Trinidad And Tobago","TN":"Tunisia","TR":"Turkey","TM":"Turkmenistan","TC":"Turks And Caicos Islands","TV":"Tuvalu","UG":"Uganda","UA":"Ukraine","AE":"United Arab Emirates","GB":"United Kingdom","US":"United States","UM":"United States Outlying Islands","UY":"Uruguay","UZ":"Uzbekistan","VU":"Vanuatu","VE":"Venezuela","VN":"Viet Nam","VG":"Virgin Islands, British","VI":"Virgin Islands, U.S.","WF":"Wallis And Futuna","EH":"Western Sahara","YE":"Yemen","ZM":"Zambia","ZW":"Zimbabwe"});
+function composeTimeStamp(date: Date, locale: string): string {
+    const prefix: string = "######"
+    const weekDayName: string = getTodayWeekDayName(date);
+    const monthName: string = getTodayMonthName(date);
+    const monthDay: string = date.getDate().toString();
+    const time: string = date.toLocaleTimeString(locale, { hour12: false });
+    const timeZoneAbbr: string = getTimeZoneAbbreviation();
+    const year: string = date.getFullYear().toString();
     return concatWithDelimiter(" ", [prefix, weekDayName, monthName, monthDay, time, timeZoneAbbr, year]);
 }
 
-function geTodayMonthName(date: Date): string {
+function getTodayMonthName(date: Date): string {
     // let fullNameList: string[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    let shortNameList: string[] = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-    let targetIndex: number = date.getMonth();
+    const shortNameList: string[] = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    const targetIndex: number = date.getMonth();
     return (shortNameList[targetIndex]);
 }
 
 function getTodayWeekDayName(date: Date): string {
     // let fullNameList: string[] = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    let shortNameList: string[] = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-    let targetIndex: number = date.getDay();
+    const shortNameList: string[] = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    const targetIndex: number = date.getDay();
     return (shortNameList[targetIndex]);
 }
 
-function getTimeZoneAbbreviation(): string {
-    let timezone = require('moment-timezone');
-    let userTimeZone: string = timezone.tz.guess();
+function getTimeZoneAbbreviation(): string {    
+    const userTimeZone: string = timezone.tz.guess();
     return timezone.tz(userTimeZone).zoneAbbr();
 }
 
 function concatWithDelimiter(delimiter: string, stringList: string[]): string {
     let result: string[] = [];
-    for (var i = 0; i < stringList.length - 1; i++) {
+    for (let i = 0; i < stringList.length - 1; i++) {
         result.push(stringList[i]);
         result.push(delimiter);
     }
@@ -42,23 +42,25 @@ function concatWithDelimiter(delimiter: string, stringList: string[]): string {
     return result.join("");
 }
 
-function replaceEditorSelection(text: string) {
+function replaceEditorSelection(locale: string) {
+    const timeStamp: string = composeTimeStamp(new Date(), locale);
     const editor = vscode.window.activeTextEditor;
     const selections = editor.selections;
 
     editor.edit((editBuilder) => {
         selections.forEach((selection) => {
             editBuilder.replace(selection, '');
-            editBuilder.insert(selection.active, text);
+            editBuilder.insert(selection.active, timeStamp);
         });
     });
+
 }
 
 export function activate(context: vscode.ExtensionContext) {
     let disposable = [
-        vscode.commands.registerCommand('inserttimestamp.perform', () => {
-            let timeStamp: string = composeTimeStamp(new Date());
-            replaceEditorSelection(timeStamp);
+        vscode.commands.registerCommand('inserttimestamp.perform', () => {  
+            vscode.window.showQuickPick(ISOCountries)
+            .then(replaceEditorSelection);                      
         })
     ];
     context.subscriptions.push(...disposable);
--- typings/node.d.ts
+++ typings/node.d.ts
@@ -1,1 +1,5 @@
 /// <reference path="../node_modules/vscode/typings/node.d.ts" />
+declare module "*.json" {
+    const value: any;
+    export default value;
+}
